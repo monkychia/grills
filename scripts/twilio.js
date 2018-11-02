418c769a4a02be5e39cfd9f1fdb46ef8c96f6726
monkychia <cchan@glyde.com>
2018-11-02 15:59:04 -0700

    Update twilio.js

--- scripts/twilio.js
+++ scripts/twilio.js
@@ -1,7 +1,7 @@
 const Twilio = require('twilio');
 
-const accountSid = 'AC2a9ce7bea93d1059cca6ebae39e88035'; // Your Account SID from www.twilio.com/console
-const authToken = '3a3a241c23959675c524b48257e98295';   // Your Auth Token from www.twilio.com/console
+const accountSid = ''; // Your Account SID from www.twilio.com/console
+const authToken = '';   // Your Auth Token from www.twilio.com/console
 
 // const twilio = require('twilio');
 const client = new Twilio(accountSid, authToken);
